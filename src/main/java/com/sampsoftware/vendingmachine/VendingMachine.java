c2e70ab6989c7d48ed7bb8e51f5b6a317cd83961
Chris Samp <chrisgsamp@gmail.com>
2015-10-06 19:15:34 -0400

    Added foreign object test, dime value test

--- src/main/java/com/sampsoftware/vendingmachine/VendingMachine.java
+++ src/main/java/com/sampsoftware/vendingmachine/VendingMachine.java
@@ -1,8 +1,38 @@
-public class VendingMachine {
+package com.sampsoftware.vendingmachine;
 
-	public boolean insertCoin(Object coin) {
-		if (coin == null)
+import com.sampsoftware.vendingmachine.coins.*;
+
+public class VendingMachine {
+	private int balanceInCents = 0;
+
+	private static int findCoinValue(Coin coin) {
+		int value = 0;
+		switch (coin.getWeight()) {
+			case 2268:
+				value = 10;
+				break;
+		}
+		return value;
+	}
+
+	public boolean insertCoin(Object inserted) {
+		if (inserted == null)
 			return false;
+		
+		if (!(inserted instanceof Coin))
+			return false;
+
+		Coin coin = (Coin)inserted;
+
+		int value = findCoinValue(coin);
+		if (value == 0)
+			return false;
+		
+		balanceInCents += value;
 		return true;
+	}
+
+	public int getBalance() {
+		return balanceInCents;
 	}
 }
--- src/test/java/com/sampsoftware/vendingmachine/VendingMachineTest.java
+++ src/test/java/com/sampsoftware/vendingmachine/VendingMachineTest.java
@@ -1,5 +1,5 @@
+import com.sampsoftware.vendingmachine.VendingMachine;
 import com.sampsoftware.vendingmachine.coins.*;
-
 import org.junit.Test;
 import static junit.framework.TestCase.assertEquals;
 
@@ -11,9 +11,23 @@ public class VendingMachineTest {
 	}
 
 	@Test
+	public void whenNonCoinIsInsertedRejectCoin() {
+		VendingMachine vendingMachine = new VendingMachine();
+		assertEquals(false,vendingMachine.insertCoin(new Object()));
+	}
+
+	@Test
 	public void whenDimeIsInsertedAcceptDime() {
 		VendingMachine vendingMachine = new VendingMachine();
 		Coin coin = new Dime();
 		assertEquals(true,vendingMachine.insertCoin(coin));
+	}
+
+	@Test
+	public void whenDimeIsInsertedIncrementBalance() {
+		VendingMachine vendingMachine = new VendingMachine();
+		Coin coin = new Dime();
+		vendingMachine.insertCoin(coin);
+		assertEquals(10,vendingMachine.getBalance());
 	}
 }
